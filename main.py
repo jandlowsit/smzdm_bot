5231210e5f3501b414a90ce5ad7b4ffb02004db5
jandlowsit <280348905@qq.com>
2021-03-12 21:44:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from sys import argv
 
 import config
 from utils.serverchan_push import push_to_wechat
+import json
 
 class SMZDM_Bot(object):
     def __init__(self):
@@ -22,7 +23,6 @@ class SMZDM_Bot(object):
         """
         try:
             result = msg.json()
-            print(result)
             return True
         except Exception as e:
             print(f'Error : {e}')            
@@ -34,6 +34,7 @@ class SMZDM_Bot(object):
         cookie 为浏览器复制来的字符串
         :param cookie: 登录过的社区网站 cookie
         """
+        cookies = cookies.encode("utf-8").decode("latin1")
         self.session.headers['Cookie'] = cookies    
 
     def checkin(self):
@@ -43,20 +44,29 @@ class SMZDM_Bot(object):
         url = 'https://zhiyou.smzdm.com/user/checkin/jsonp_checkin'
         msg = self.session.get(url)
         if self.__json_check(msg):
-            return msg.json()
+            return self.msgFormat(msg.json())
         return msg.content
+
+    def msgFormat(self, msg):
+        s = msg.get("data")
+        _res = "当前积分："+str(s.get("point"))+"\n"
+        _res += "当前经验值："+str(s.get("exp"))+"\n"
+        _res += "签到天数："+str(s.get("checkin_num"))+"\n"
+        return _res
+
 
 
 
 
 if __name__ == '__main__':
     sb = SMZDM_Bot()
-    # sb.load_cookie_str(config.TEST_COOKIE)
+    sb.load_cookie_str(config.TEST_COOKIE)
     cookies = os.environ["COOKIES"]
     SERVERCHAN_SECRETKEY = os.environ["SERVERCHAN_SECRETKEY"]
-    sb.load_cookie_str(cookies)
+    # sb.load_cookie_str(cookies)
+    # SERVERCHAN_SECRETKEY = config.SERVERCHAN_SECRETKEY
+
     res = sb.checkin()
-    print(res)
     push_to_wechat(text = '什么值得买每日签到',
                     desp = str(res),
                     secretKey = SERVERCHAN_SECRETKEY)
